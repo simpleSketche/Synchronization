47f8480862224143132262d657fb4eea4eead3cf
stadwalkar <sahiltadwalkar@gmail.com>
2021-11-06 15:05:11 -0700

    web socket client

---
+++ c_sharp_client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+
+
+namespace c_sharp_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (WebSocket ws = new WebSocket("ws://simple-websocket-server-echo.glitch.me/"))
+                
+            { 
+                ws.OnMessage += Ws_OnMessage;
+                ws.Connect();
+                ws.Send("Hello Server");
+                Console.ReadKey();
+            }
+        }
+     
+          
+
+        private static void Ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine("Recieved from the server: " + e.Data);        
+        }
+    }
+}
