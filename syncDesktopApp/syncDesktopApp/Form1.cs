63382e52060860f304bad3aeeafb99a01c2b6604
rfeathers068 <rfeathers@thorntontomasetti.com>
2021-11-07 09:04:27 -0500

    Updates to Json Storage

--- syncDesktopApp/syncDesktopApp/Form1.cs
+++ syncDesktopApp/syncDesktopApp/Form1.cs
@@ -10,6 +10,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Newtonsoft.Json;
+using ETABSv1;
+using static ETABS_Globals;
 
 namespace syncDesktopApp
 {
@@ -46,14 +48,69 @@ namespace syncDesktopApp
             return response;
         }
 
+        private class BeamStorageGUID
+        {
+            private Dictionary<string, BeamStorageProperties> beamProperties = new Dictionary<string, BeamStorageProperties>();
+
+            public BeamStorageGUID(string GUID, BeamStorageProperties beamProp)
+            {
+                beamProperties.Add(GUID, beamProp);
+            }
+
+        }
+
+        private class BeamStorageProperties
+        {
+            private string uniqueName;
+            private string startPt;
+            private string endPt;
+            private string section;
+            private bool status;
+
+            public BeamStorageProperties(string UniqueName, string StartPt, string EndPt, string Section, bool Status)
+            {
+                uniqueName = UniqueName;
+                startPt = StartPt;
+                endPt = EndPt;
+                section = Section;
+                status = Status;
+            }
+        }
+
         private async Task<string> updateVal()
         {
-            
+            // Attach to ETABS COM object
+            cSapModel mySapModel;
+            //cPluginCallback ISapPlugin;
+            cOAPI myETABSObject;
+            cHelper myHelper;
+            myHelper = new Helper();
+            myETABSObject = myHelper.GetObject("CSI.ETABS.API.ETABSObject");
+            mySapModel = myETABSObject.SapModel;
+
+            // 0 -- Initialize JSON File and Post to web
+            //{GUID: {	UniqueName: ?, Start Point:	?, End Point:		?, Section:        ?, Status:         ?}}
+            string GUID = "";
+
+            mySapModel.FrameObj.GetAllFrames(ref NumberofFrames, ref MyName, ref PropName, ref StoryName, ref PointName1, ref PointName2, ref Point1X, ref Point1Y, ref Point1Z, ref Point2X, ref Point2Y, ref Point2Z, ref Angle, ref Offset1X, ref Offset2X, ref Offset1Y, ref Offset2Y, ref Offset1Z, ref Offset2Z, ref CardinalPoint);
+            BeamStorageGUID[] beamJSON = new BeamStorageGUID[NumberofFrames];
+            for (int i = 0; i < NumberofFrames; i++)
+            {
+                mySapModel.FrameObj.GetGUID(MyName[i], ref GUID);
+                BeamStorageProperties beamProp = new BeamStorageProperties(MyName[i], PointName1[i], PointName2[i], PropName[i], false);
+                beamJSON[i] = new BeamStorageGUID(GUID, beamProp);
+            }
+
+
             while (true)
             {
-
                 
-                this.inputVal++;
+                // 1 -- Search for most recent locked items
+                // 2 -- Search through locked items list
+                // 3 -- Search for changes to ETABS model
+                // 4 -- Sleep for 10 seconds
+
+
 
                 WebResponse response = await Task.Run(() => GetResp());
 
@@ -62,10 +119,8 @@ namespace syncDesktopApp
                 receiver.Clear();
                 receiver.AppendText(responseJson.ToString());
 
-                if (this.inputVal == 6)
-                {
-                    break;
-                }
+                System.Threading.Thread.Sleep(3000);
+
             }
             return "sucess!";
         }
